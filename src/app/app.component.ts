83bcf945dd0c34134f997760af429e33ec8e8d71
Sinan <sinanfen@hotmail.com>
2021-04-01 16:24:20 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //index sayfasında <app-root></app-root> yazılmasını sağlayan kısımdır.
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'northwind';
+  title = 'northwind'; //bu satırın TScript deki yazılımı title:string = 'northwind'; şeklindedir.
+  user: string = "Sinan Fen";
+  
 }
+
+//COMPONENT : HTML'NIN DATASINI KONTROL ETTIGIMIZ YER.
---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductService } from 'src/app/services/product.service';
+
+//axios , fetch
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  dataLoaded = false;
+  filterText = '';
+
+  constructor(
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute,
+    private toastrService: ToastrService,
+    private cartService:CartService
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['categoryId']) {
+        this.getProductsByCategory(params['categoryId']);
+      } else {
+        this.getProducts();
+      }
+    });
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe((response) => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    });
+  }
+
+  getProductsByCategory(categoryId: number) {
+    this.productService
+      .getProductsByCategory(categoryId)
+      .subscribe((response) => {
+        this.products = response.data;
+        this.dataLoaded = true;
+      });
+  }
+
+  addToCart(product: Product) {
+    if (product.unitsInStock === 0) {
+      this.toastrService.error('Bu ürün stokta bulunmuyor', 'Hata');
+    } else {
+      this.toastrService.success('Sepete eklendi', product.productName);
+      this.cartService.addToCart(product);
+    }
+  }
+}
